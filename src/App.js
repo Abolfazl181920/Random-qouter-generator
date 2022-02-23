934c27eabd616fc5d04dd1d79337a5ada1c148ea
Abolfazl <abolfazlm181920@gmail.com>
2022-02-23 16:39:59 +0330

    get data from api with axios

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ class App extends Component {
     state = { advice: '' };
 
     componentDidMount() {
-        
+        this.fetchData();
+    }
+
+    fetchData = () => {
+        axios.get('https://api.adviceslip.com/advice')
+            .then((response) => {
+                const { advice } = response.data.slip;
+                console.log(advice);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
     render() {
         return(
